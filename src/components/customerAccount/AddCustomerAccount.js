b74096b0f311ae662529d5c4a7477d8fa99817da
Lmarl Saria <lmarlsaria21@gmail.com>
2022-10-03 19:27:08 +0800

    add customer account

--- src/components/customerAccount/AddCustomerAccount.js
+++ src/components/customerAccount/AddCustomerAccount.js
@@ -183,132 +183,123 @@ function AddCustomerAccount() {
           <div className="container p-4">
             <div className="row p-5">
               <div className="card col-md-8 offset-md-2">
-                <h3 className="text-center  mt-4">
+                <h3 className="text-center mt-3">
                   <b>Create Customer Account</b>
                 </h3>
                 <div className="card-body">
-                  <form>
-                    <div className="form-group mb-3">
-                      <label> Account No: </label>
-                      <div className="row">
-                        <div className="w-1/2">
-                          <input
-                            placeholder="Account Number"
-                            name="acctNo"
-                            value={acctNo}
-                            onChange={changeInput}
-                            maxLength={4}
-                            className="form-control"
+                  <div class="grid-cols-2 sm:grid gap-4">
+                    <input
+                      placeholder="Account Number"
+                      name="acctNo"
+                      value={acctNo}
+                      onChange={changeInput}
+                      maxLength={4}
+                      class="w-full px-4 py-2 border-2 rounded-md border-gray-400 outline-none  focus:border-gray-400 mt-3"
+                    />
+                    {isLoading === false ? (
+                      <button
+                        className="w-full py-2 text-white bg-green-500 rounded-md  focus:bg-green-600 focus:outline-none mt-3"
+                        onClick={validateAccountNumber}
+                      >
+                        <i
+                          className="fa-solid fa-square-check mr-3"
+                          style={{ marginRight: "10px" }}
+                        />
+                        <span className="ml-3">Verify</span>
+                      </button>
+                    ) : (
+                      <button
+                        className="w-full py-2 text-white bg-green-300 rounded-md focus:outline-none mt-3"
+                        onClick={validateAccountNumber}
+                        disabled={true}
+                      >
+                        <span className="text-white">
+                          <Spinner
+                            animation="border"
+                            variant="light"
+                            size="sm"
+                            style={{
+                              marginRight: "10px",
+                            }}
                           />
-                        </div>
-                        <div className="w-full">
-                          {isLoading === false ? (
-                            <button
-                              className="btn btn-success w-1/2"
-                              onClick={validateAccountNumber}
-                            >
-                              <i
-                                className="fa-solid fa-square-check mr-3"
-                                style={{ marginRight: "10px" }}
-                              />
-                              <span className="ml-3">Verify</span>
-                            </button>
-                          ) : (
-                            <button
-                              className="col-12 btn btn-success btn-block"
-                              onClick={validateAccountNumber}
-                              disabled={true}
-                            >
-                              <span className="text-white">
-                                <Spinner
-                                  animation="border"
-                                  variant="light"
-                                  size="sm"
-                                  style={{
-                                    marginRight: "10px",
-                                  }}
-                                />
-                              </span>
-                              <span className="ml-3">Verify</span>
-                            </button>
-                          )}
-                        </div>
-                      </div>
-                    </div>
-                    <div className="form-group mb-3">
-                      <label> First Name: </label>
+                        </span>
+                        <span className="ml-3">Verify</span>
+                      </button>
+                    )}
+                  </div>
+                  <div class="grid-cols-2 sm:grid gap-4">
+                    <div className="form-group">
                       <input
                         placeholder="First Name"
                         name="firstName"
                         value={firstName}
                         onChange={changeInput}
-                        className="form-control"
+                        className="w-full px-4 py-2 border-2 rounded-md border-gray-400 outline-none  focus:border-gray-400 mt-3"
                       />
                     </div>
-                    <div className="form-group mb-3">
-                      <label> Last Name: </label>
+                    <div className="form-group">
                       <input
                         placeholder="Last Name"
                         name="lastName"
                         value={lastName}
                         onChange={changeInput}
-                        className="form-control"
+                        className="w-full px-4 py-2 border-2 rounded-md border-gray-400 outline-none  focus:border-gray-400 mt-3"
                       />
                     </div>
-                    <div className="form-group mb-3">
-                      <label> Address: </label>
+                  </div>
+                  <div class="grid-cols-1 sm:grid gap-2 mt-2">
+                    <div className="form-group">
                       <input
                         placeholder="Address"
                         name="address"
                         value={address}
                         onChange={changeInput}
-                        className="form-control"
+                        className="w-full px-4 py-2 border-2 rounded-md border-gray-400 outline-none  focus:border-gray-400"
                       />
                     </div>
-                    <div className="d-flex justify-content-between">
-                      {save ? (
-                        <button
-                          className="btn btn-success mt-3 col-5"
-                          onClick={validate}
-                          disabled
-                        >
-                          <span className="text-white">
-                            <Spinner
-                              animation="border"
-                              variant="light"
-                              size="sm"
-                              style={{
-                                marginRight: "10px",
-                              }}
-                            />
-                          </span>
-                          <span className="ml-3">Save</span>
-                        </button>
-                      ) : (
-                        <button
-                          className="btn btn-success mt-3 col-6"
-                          onClick={validate}
-                        >
-                          <i
-                            className="fa-solid fa-square-check mr-3"
-                            style={{ marginRight: "10px" }}
-                          />
-                          <span className="ml-3">Save</span>
-                        </button>
-                      )}
+                  </div>
+                  <div class="grid-cols-2 sm:grid gap-4">
+                    {save ? (
                       <button
-                        className="btn btn-danger mt-3 col-6"
-                        onClick={reset}
-                        style={{ marginLeft: "10px" }}
+                        className="w-full   py-2 text-white bg-green-300 focus:bg-green-600 rounded-md focus:outline-none mt-3"
+                        onClick={validate}
+                        disabled
+                      >
+                        <span className="text-white">
+                          <Spinner
+                            animation="border"
+                            variant="light"
+                            size="sm"
+                            style={{
+                              marginRight: "10px",
+                            }}
+                          />
+                        </span>
+                        <span className="ml-3">Save</span>
+                      </button>
+                    ) : (
+                      <button
+                        className="w-full   py-2 text-white bg-green-500 focus:bg-green-600 rounded-md focus:outline-none mt-3"
+                        onClick={validate}
                       >
                         <i
-                          className="fa-solid fa-rotate-right"
+                          className="fa-solid fa-square-check mr-3"
                           style={{ marginRight: "10px" }}
                         />
-                        Reset
+                        <span className="ml-3">Save</span>
                       </button>
-                    </div>
-                  </form>
+                    )}
+                    <button
+                      className="w-full py-2 text-white bg-red-700 rounded-md focus:outline-none mt-3"
+                      onClick={reset}
+                    >
+                      <i
+                        className="fa-solid fa-rotate-right"
+                        style={{ marginRight: "10px" }}
+                      />
+                      Reset
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
@@ -320,3 +311,121 @@ function AddCustomerAccount() {
 }
 
 export default AddCustomerAccount;
+{
+  /* <form>
+  <div className="form-group mb-3">
+    <label> Account No: </label>
+    <div className="row">
+      <div className="w-1/2">
+        <input
+          placeholder="Account Number"
+          name="acctNo"
+          value={acctNo}
+          onChange={changeInput}
+          maxLength={4}
+          className="form-control"
+        />
+      </div>
+      <div className="w-full">
+        {isLoading === false ? (
+          <button
+            className="btn btn-success w-1/2"
+            onClick={validateAccountNumber}
+          >
+            <i
+              className="fa-solid fa-square-check mr-3"
+              style={{ marginRight: "10px" }}
+            />
+            <span className="ml-3">Verify</span>
+          </button>
+        ) : (
+          <button
+            className="col-12 btn btn-success btn-block"
+            onClick={validateAccountNumber}
+            disabled={true}
+          >
+            <span className="text-white">
+              <Spinner
+                animation="border"
+                variant="light"
+                size="sm"
+                style={{
+                  marginRight: "10px",
+                }}
+              />
+            </span>
+            <span className="ml-3">Verify</span>
+          </button>
+        )}
+      </div>
+    </div>
+  </div>
+  <div className="form-group mb-3">
+    <label> First Name: </label>
+    <input
+      placeholder="First Name"
+      name="firstName"
+      value={firstName}
+      onChange={changeInput}
+      className="form-control"
+    />
+  </div>
+  <div className="form-group mb-3">
+    <label> Last Name: </label>
+    <input
+      placeholder="Last Name"
+      name="lastName"
+      value={lastName}
+      onChange={changeInput}
+      className="form-control"
+    />
+  </div>
+  <div className="form-group mb-3">
+    <label> Address: </label>
+    <input
+      placeholder="Address"
+      name="address"
+      value={address}
+      onChange={changeInput}
+      className="form-control"
+    />
+  </div>
+  <div className="d-flex justify-content-between">
+    {save ? (
+      <button
+        className="btn btn-success mt-3 col-5"
+        onClick={validate}
+        disabled
+      >
+        <span className="text-white">
+          <Spinner
+            animation="border"
+            variant="light"
+            size="sm"
+            style={{
+              marginRight: "10px",
+            }}
+          />
+        </span>
+        <span className="ml-3">Save</span>
+      </button>
+    ) : (
+      <button className="btn btn-success mt-3 col-6" onClick={validate}>
+        <i
+          className="fa-solid fa-square-check mr-3"
+          style={{ marginRight: "10px" }}
+        />
+        <span className="ml-3">Save</span>
+      </button>
+    )}
+    <button
+      className="btn btn-danger mt-3 col-6"
+      onClick={reset}
+      style={{ marginLeft: "10px" }}
+    >
+      <i className="fa-solid fa-rotate-right" style={{ marginRight: "10px" }} />
+      Reset
+    </button>
+  </div>
+</form>; */
+}
